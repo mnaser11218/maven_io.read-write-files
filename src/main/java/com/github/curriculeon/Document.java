f77c2e0e341b8da39daa56a3d58ce02d4ff1a512
Mohammed N <mnaser11218@gmail.com>
2024-10-21 21:51:25 -0400

    ...

--- src/main/java/com/github/curriculeon/Document.java
+++ src/main/java/com/github/curriculeon/Document.java
@@ -1,6 +1,8 @@
 package com.github.curriculeon;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -9,6 +11,7 @@ import java.util.List;
 public class Document implements DocumentInterface {
 
     private final FileWriter fileWriter;
+    private final FileReader fileReader;
     private final File file;
 
     public Document(String fileName) throws IOException {
@@ -18,24 +21,53 @@ public class Document implements DocumentInterface {
     public Document(File file) throws IOException {
         this.file = file;
         this.fileWriter = new FileWriter(file);
+        this.fileReader = new FileReader(file);
     }
 
     @Override
-    public void write(String contentToBeWritten) {
+    public void write(String contentToBeWritten) throws IOException {
+        fileWriter.write(contentToBeWritten);
+        fileWriter.close();
     }
 
     @Override
-    public void write(Integer lineNumber, String valueToBeWritten) {
+    public void write(Integer lineNumber, String valueToBeWritten) throws IOException {
+//        fileWriter.write(lineNumber valueToBeWritten);
+       // fileWriter.close();
+        /// testing
+
+
     }
 
     @Override
-    public String read(Integer lineNumber) {
-        return null;
+    public String read(Integer lineNumber) throws IOException {
+        String line = Files.readAllLines(Paths.get(file.toURI())).get(lineNumber);
+        return line;
+//        FileInputStream fs= new FileInputStream(file);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+//        for(int i = 0; i < lineNumber-1; i++) {
+//
+//            br.readLine();
+//            String lineIWant = br.readLine();
+//            if(i == lineNumber-1){
+//                return lineIWant;
+//            }
+//
+//        }
+//
+//        return null;
     }
 
     @Override
-    public String read() {
-        return null;
+    public String read() throws IOException {
+        int i;
+        String results = "";
+        while ((i = fileReader.read()) != -1) {
+            System.out.print((char)i);
+            results += (char)i;
+        }
+        return results;
+
     }
 
     @Override
